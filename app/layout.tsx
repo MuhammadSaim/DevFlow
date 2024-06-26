8d790ccee8d0ace9dcfcf769ff0031ff65eda528
Muhammad Saim <muhammadsaim494@gmail.com>
2024-06-26 22:27:24 +0500

    feat: ThemeProvider or ligh and dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 // import styles
 import "./globals.css";
+import ThemeProvider from "@/context/ThemeProvider";
 
 
 const inter = Inter({ 
@@ -35,20 +36,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{
-          elements: {
-            formButtonPrimary: 'primary-gradient',
-            footerActionLink: 'primary-text-gradient hover:text-primary-500'
-          },
-        }
-      }
-    >
       <html lang="en">
         <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
-          {children}
+          <ClerkProvider
+            appearance={{
+                elements: {
+                  formButtonPrimary: 'primary-gradient',
+                  footerActionLink: 'primary-text-gradient hover:text-primary-500'
+                },
+              }
+            }
+          >
+            <ThemeProvider>
+              {children}
+            </ThemeProvider>
+          </ClerkProvider>
         </body>
       </html>
-    </ClerkProvider>
   );
 }
